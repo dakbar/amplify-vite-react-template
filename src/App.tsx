6b1514b61673ce7c67e2cdb384ad99dd9336289b
root <root@ip-10-134-52-93.ec2.internal>
2024-08-27 07:18:33 +0000

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       {({ signOut, user }) => (
 
     <main>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
@@ -55,7 +55,7 @@ function App() {
     </main>
     
       )}
-    </Authenticator>
+   </Authenticator>
 
   );
 }
